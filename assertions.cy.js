77a87cf5e59fa2d4b139c3b9f232ebd6c6e378d2
mbgshk <mbgshk@gmail.com>
2022-07-08 12:39:24 +0700

    Check the URL

--- assertions.cy.js
+++ assertions.cy.js
@@ -1,13 +1,24 @@
 /// <reference types="cypress" />
 
 // Sample
-describe('My First Test', () => {
-    it('clicking "type" navigates to a new url', () => {
-      cy.visit('https://example.cypress.io')
+// describe('My First Test', () => {
+//     it('clicking "type" navigates to a new url', () => {
+//       cy.visit('https://example.cypress.io')
       
-      cy.contains('type').click()
+//       cy.contains('type').click()
 
-      // Should be on a new URL which includes '/commands/actions'
-      cy.url().should('include', '/commands/actions')
-    })
-})
+//       // Should be on a new URL which includes '/commands/actions'
+//       cy.url().should('include', '/commands/actions')
+//     })
+// })
+
+// Check the URL
+describe('Browser Actions', () => {
+    it('Should load corret url', () => {
+        cy.visit('https://example.com/', { timeout: 10000 })
+    });
+
+    it('Should check correct url', () => {
+        cy.url().should('include', 'example.com')
+    });
+});
