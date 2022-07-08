9e051f885898d5f9fbdbb7d2e0b0877bbc4505d9
mbgshk <mbgshk@gmail.com>
2022-07-08 12:34:25 +0700

    Insert Sample

--- assertions.cy.js
+++ assertions.cy.js
@@ -1,2 +1,13 @@
 /// <reference types="cypress" />
 
+// Sample
+describe('My First Test', () => {
+    it('clicking "type" navigates to a new url', () => {
+      cy.visit('https://example.cypress.io')
+      
+      cy.contains('type').click()
+
+      // Should be on a new URL which includes '/commands/actions'
+      cy.url().should('include', '/commands/actions')
+    })
+})
